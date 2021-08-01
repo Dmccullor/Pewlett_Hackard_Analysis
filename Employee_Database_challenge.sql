9dcb89b116964b9bd2babd2a8727d30cf6b61808
dmccullor <dmccullor0@gmail.com>
2021-07-31 19:09:17 -0500

    Correcting syntax

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -14,9 +14,9 @@ ORDER BY e.emp_no;
 
 -- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (rt.emp_no) rt.title,
-rt.emp_no,
-rt.first_name,
-rt.last_name
+	rt.emp_no,
+	rt.first_name,
+	rt.last_name
 INTO unique_titles
 FROM retirement_titles AS rt
 ORDER BY rt.emp_no, rt.to_date DESC;
@@ -36,12 +36,12 @@ SELECT DISTINCT ON (e.emp_no)
 	t.title,
 	de.from_date,
 	de.to_date
-	INTO mentorship_eligibility
-	FROM employees AS e
-	JOIN dept_emp AS de
-	ON e.emp_no = de.emp_no
-	JOIN titles AS t
-	ON t.emp_no = e.emp_no
-	WHERE t.to_date = '9999-01-01'
-	AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
-	ORDER BY e.emp_no;
+INTO mentorship_eligibility
+FROM employees AS e
+JOIN dept_emp AS de
+ON e.emp_no = de.emp_no
+JOIN titles AS t
+ON t.emp_no = e.emp_no
+WHERE t.to_date = '9999-01-01'
+AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+ORDER BY e.emp_no;
